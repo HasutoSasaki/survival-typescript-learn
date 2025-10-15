d6fc226ce409256aeba0ba2053486a5c4eea0e4c
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-16 06:37:49 +0900

    update: 構造的型付けから、名前的型付けにする方法. - privateメンバーを持つクラスは、他のクラスと区別される。これはprivateメンバーがそのクラス固有のものなため.

--- src/examples/05-structural-typing.ts
+++ src/examples/05-structural-typing.ts
@@ -10,3 +10,30 @@ class Dog {
 const person = new Person()
 const dog: Dog = person
 
+// 構造的型付けから名前的型付けにする方法
+class UserId {
+    private id: string
+
+    constructor(id: string) {
+        this.id = id
+    }
+
+    getId(): string {
+        return this.id
+    }
+}
+
+class ProductId {
+    private id: string
+
+    constructor(id: string) {
+        this.id = id
+    }
+
+    getId(): string {
+        return this.id
+    }
+}
+
+const userId: UserId = new UserId("1")
+// const productId: ProductId = userId 代入エラーになる
