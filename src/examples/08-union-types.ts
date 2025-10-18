1098748d74d0f0eeb1ac77d72f79a8c838c9e54e
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-19 08:53:51 +0900

    add: ユニオン型の例と、判別可能なディスクリミネータ追加.

---
+++ src/examples/08-union-types.ts
@@ -0,0 +1,35 @@
+// 通常のユニオンは絞り込みが複雑になる
+type UploadStatus = InProgress | Success | Failure;
+type InProgress = { done: boolean, progress: number }
+type Success = { done: boolean }
+type Failure = { done: boolean; error: Error }
+
+function printStatus(status: UploadStatus) {
+    if (status.done) {
+        if ("error" in status) {
+            console.log(`アップロード失敗：${status.error.message}`)
+        } else {
+            console.log("アップロード成功")
+        }
+    } else if ("progress" in status) {
+        console.log(`アップロード中：${status.progress}%`)
+    }
+}
+
+// 判別可能なユニオン型：ディスクリミネータ
+type UploadStatus2 = InProgress2 | Success2 | Failure2
+type InProgress2 = { type: "InProgress"; progress: number }
+type Success2 = { type: "Success" }
+type Failure2 = { type: "Failure"; error: Error }
+
+function printStatus2(status: UploadStatus2) {
+    if (status.type === "InProgress") {
+        console.log(`アップロード中：${status.progress}%`)
+    } else if (status.type === "Success") {
+        console.log("アップロード成功", status)
+    } else if (status.type === "Failure") {
+        console.log(`アップロード失敗：${status.error.message}`);
+    } else {
+        console.log("不正なステータス：", status)
+    }
+}
