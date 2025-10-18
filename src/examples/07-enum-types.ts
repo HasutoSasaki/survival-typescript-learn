214f17e9bf0c07f07d02d18c82a1606e55648fb6
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-19 08:24:08 +0900

    update: 列挙型の代替案追加.

--- src/examples/07-enum-types.ts
+++ src/examples/07-enum-types.ts
@@ -19,3 +19,53 @@ enum Position2 {
     Bottom, // 3
     Left // 4
 }
+
+// 列列挙の代替案
+// 1: ユニオン型
+type YesNo = "yes" | "no";
+
+function toJapanese(yesno: YesNo) {
+    switch (yesno) {
+        case "yes":
+            return "はい";
+        case "no":
+            return "いいえ"
+    }
+}
+
+// ユニオン & シンボル
+const yes = Symbol();
+const no = Symbol();
+type YesNo2 = typeof yes | typeof no;
+
+function toJapanese2(yesno: YesNo2) {
+    switch (yesno) {
+        case yes:
+            return "はい";
+        case no:
+            return "いいえ";
+    }
+}
+
+// オブジェクトリテラル
+const Position3 = {
+    Top: 0,
+    Right: 1,
+    Bottom: 2,
+    Left: 3,
+} as const;
+
+type Position3 = (typeof Position)[keyof typeof Position];
+
+function toJapanese3(position: Position) {
+    switch (position) {
+        case Position.Top:
+            return "上"
+        case Position.Right:
+            return "右"
+        case Position.Bottom:
+            return "下"
+        case Position.Left:
+            return "左"
+    }
+}
