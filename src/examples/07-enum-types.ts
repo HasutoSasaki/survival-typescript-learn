374b97307208171ee2d5cf0282054f18740067b4
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-18 22:57:09 +0900

    add: enumの基本を追加.

---
+++ src/examples/07-enum-types.ts
@@ -0,0 +1,12 @@
+enum Position {
+    Top,
+    Right,
+    Bottom,
+    Left
+}
+
+// 列挙型のメンバーオブジェクトのプロパティは連番になる
+console.log(Position.Top) // 0
+console.log(Position.Right) // 1
+console.log(Position.Bottom) // 2
+console.log(Position.Left) // 3
