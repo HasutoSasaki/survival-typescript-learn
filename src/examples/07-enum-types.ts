4dd424e3b368d5b1c083b13895745882656fbe74
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-18 22:58:52 +0900

    update: 数値列挙型の例追加.

--- src/examples/07-enum-types.ts
+++ src/examples/07-enum-types.ts
@@ -10,3 +10,12 @@ console.log(Position.Top) // 0
 console.log(Position.Right) // 1
 console.log(Position.Bottom) // 2
 console.log(Position.Left) // 3
+
+// メンバーに値を代入する場合
+// 続くメンバーが連番になる
+enum Position2 {
+    Top = 1, // 1
+    Right, // 2
+    Bottom, // 3
+    Left // 4
+}
