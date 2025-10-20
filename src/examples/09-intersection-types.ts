73b8558d870c9224c58e4ce977a0a03b370d65e9
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-21 06:49:30 +0900

    update: RequiredとPartialを使って必須とオプショナルプロパティの管理を見やすくする.

--- src/examples/09-intersection-types.ts
+++ src/examples/09-intersection-types.ts
@@ -20,3 +20,42 @@ const p: ThreeDimensionalPoint = {
 type Never = string & number;
 
 const n: Never = "2"; // never：いかなる値も代入できない
+
+// インターセクション型を使いこなす
+
+// === システム拡大に伴ってパラメーターが巨大化
+// type Parameter = {
+//     id: string;
+//     index?: number;
+//     active: boolean;
+//     balance: number;
+//     photo?: string;
+//     age?: number;
+//     surname: string;
+//     givenName: string;
+//     company?: string;
+//     email: string;
+//     phoneNumber?: string;
+//     address?: string;
+// }
+
+type Mandatory = Required<{
+    id: string;
+    active: boolean;
+    balance: number;
+    surname: string;
+    givenName: string;
+    email: string;
+}>
+
+type Optional = Partial<{
+    index: number;
+    photo: string;
+    age: number;
+    company: string;
+    phoneNumber: string;
+    address: string;
+}>
+
+type Parameter = Mandatory & Optional
+
