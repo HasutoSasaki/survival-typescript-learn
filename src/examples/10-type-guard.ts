1f98cf0dae568f751e638465eaa49cea325882b6
HasutoSasaki <hasuto.sa@gmail.com>
2025-10-22 06:23:36 +0900

    add: typeof を用いた型ガード例追加.

---
+++ src/examples/10-type-guard.ts
@@ -0,0 +1,16 @@
+//制御フロー分析
+function showMonth(month: string | number) {
+    if (typeof month === "string") {
+        console.log(month.padStart(2, "0"));
+        return
+    }
+    console.log(month.toFixed) // 早期リターンをすることで、number型に絞られる
+}
+
+// typeof
+function getMonth(date: string | Date | null) {
+    if (typeof date === "object" && date != null) {
+        console.log(date.getMonth() + 1)
+    }
+}
+
