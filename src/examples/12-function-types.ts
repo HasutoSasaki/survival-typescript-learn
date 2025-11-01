c51741ee41603c636fff4e882a6a46aaa65cf724
HasutoSasaki <hasuto.sa@gmail.com>
2025-11-01 09:28:36 +0900

    update: option objectにおける、デフォルト引数の型注釈と、option object自体をオプショナルにする方法を追加.

--- src/examples/12-function-types.ts
+++ src/examples/12-function-types.ts
@@ -243,3 +243,19 @@ func5({ x: 1, y: 2, z: 3 })
 function func6({ x, y, z }: { x: number, y: number, z: number }) {
     console.log(x, y, z)
 }
+
+// デフォルト引数の型注釈
+function func7({ x, y = 0, z = 0 }: { x: number; y?: number; z?: number }) {
+    console.log(x, y, z)
+}
+
+//Option Object 自体をオプショナルにする方法
+type Options = {
+    x?: number;
+    y?: number;
+    z?: number;
+}
+
+function func8({ x = 0, y = 0, z = 0 }: Options = {}) {
+    console.log(x, y, z);
+}
