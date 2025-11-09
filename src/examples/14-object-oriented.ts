860073830f786b60f7a10bd51b3b31da9562e034
HasutoSasaki <hasuto.sa@gmail.com>
2025-11-10 06:47:26 +0900

    update: protectedの例を追加.

--- src/examples/14-object-oriented.ts
+++ src/examples/14-object-oriented.ts
@@ -21,3 +21,30 @@ gorilla.move(10);
 "ゴリラ moved 10m."
 gorilla.name = "ゴリラゴリラ";
 gorilla.move(20);
+
+// protected
+class Animal2 {
+    public name: string;
+
+    public constructor(theName: string) {
+        this.name = theName;
+    }
+
+    // public から　protected に変更
+    protected move(distanceInMeters: number) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`)
+    }
+}
+
+const cat = new Animal2("キャット")
+cat.move(10)// プロパティ 'move' は保護されているため、クラス 'Animal2' とそのサブクラス内でのみアクセスできます。
+
+// 十倍早く動くゴリラ
+class Gorilla extends Animal2 {
+    move(distanceInMeters: number) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`)
+    }
+}
+
+const fastGorilla = new Gorilla("速いゴリラ")
+fastGorilla.move(10) // 速いゴリラ moved 100m.
